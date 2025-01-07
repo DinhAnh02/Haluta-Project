83dc18a0dd9b475b194be66a9fba911c7e322f82
Chaun09 <bangcac24@gmail.com>
2025-01-07 10:27:16 +0700

    Backendfolder

---
+++ HalutaBackEnd/src/main/java/Haluta/example/demo/HalutaProjectApplication.java
@@ -0,0 +1,13 @@
+package Haluta.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalutaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalutaProjectApplication.class, args);
+	}
+
+}
---
+++ HalutaBackEnd/src/test/java/Haluta/example/demo/HalutaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Haluta.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalutaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
