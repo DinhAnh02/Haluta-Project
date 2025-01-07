614a765cfb42dd5770fcdcb50dbb9a91e8669b7b
Chaun09 <bangcac24@gmail.com>
2025-01-07 13:45:11 +0700

    SignUpSignInComponent

--- HalutaFrontEnd/src/app/app.module.ts
+++ HalutaFrontEnd/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { SignupComponent } from './layout/signup/signup.component';
+import { SigninComponent } from './layout/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
