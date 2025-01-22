f0694cefdceb2169c874e7b45830678a45eb4d78
Chaun09 <bangcac24@gmail.com>
2025-01-22 16:33:27 +0700

    FixValidDto

--- HalutaBackEnd/src/main/java/Haluta/example/demo/dto/Auth/SignUpRequest.java
+++ HalutaBackEnd/src/main/java/Haluta/example/demo/dto/Auth/SignUpRequest.java
@@ -5,9 +5,13 @@ import lombok.*;
 
 @Data
 public class SignUpRequest {
-    @NotBlank(message = "Fullname is required")
+    @NotBlank(message = "Full name is required")
+    @Pattern(regexp = "^[A-Za-zÀ-ỹ\\s]{1,255}$", message = "Full name must contain upppercase, lowercase letters and numbers, special symbol and whitespace")
+    @Size(max = 255, message = "Full name must be 255 characters long")
     private String full_name;
     @NotBlank(message = "Username is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username must contain upppercase, lowercase letters and numbers, special symbol")
+    @Size(min = 8, max = 8, message = "Username must be 8 characters long")
     private String customer_name;
     @Size(min = 8, max = 9, message = "Password must be 8-9 characters long")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password must contain upppercase, lowercase letters and numbers, special symbol")
--- HalutaBackEnd/src/main/java/Haluta/example/demo/services/auth/AuthServiceImpl.java
+++ HalutaBackEnd/src/main/java/Haluta/example/demo/services/auth/AuthServiceImpl.java
@@ -6,7 +6,6 @@ import Haluta.example.demo.enums.Role.CustomerRole;
 import Haluta.example.demo.enums.Role.UserRole;
 import Haluta.example.demo.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.Objects;
 
